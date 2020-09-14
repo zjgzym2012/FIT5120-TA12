021eb325ce746a7dd81de8daf66ba5f9f049362f
Manasie Bajpai <mbaj0002@student.monash.edu>
2020-09-15 03:35:34 +1000

    interim-commit

--- FIT5120App/Controllers/HomeController.cs
+++ FIT5120App/Controllers/HomeController.cs
@@ -56,6 +56,31 @@ namespace FIT5120App.Controllers
         {
             return View();
         }
+        public ActionResult LungCancer()
+        {
+            return View();
+        }
+        public ActionResult COPD()
+        {
+            return View();
+        }
+        public ActionResult Asbestosis()
+        {
+            return View();
+        }
+        public ActionResult Silicosis()
+        {
+            return View();
+        }
+        public ActionResult Mesothelioma()
+        {
+            return View();
+        }
+        public ActionResult Pneumoconiosis()
+        {
+            return View();
+        }
+
 
     }
 
