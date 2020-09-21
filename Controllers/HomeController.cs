ec1abe2c7d12cae12b05c7b1a1797f6c0d574355
zjgzym2012 <yzha0448@student.monash.edu>
2020-09-21 17:38:15 +1000

    Breadcrumb added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5120App.Controllers
+{
+
+    [BasicAuthenticationAttribute("your-username", "your-password", 
+    BasicRealm = "your-")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewDiseases()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Industries()
+        {
+            return View();
+        }
+        public ActionResult Mining()
+        {
+            return View();
+        }
+        public ActionResult Manufacturing()
+        {
+            return View();
+        }
+        public ActionResult Construction()
+        {
+            return View();
+        }
+        public ActionResult Asthma()
+        {
+            return View();
+        }
+        public ActionResult LungCancer()
+        {
+            return View();
+        }
+        public ActionResult COPD()
+        {
+            return View();
+        }
+        public ActionResult Asbestosis()
+        {
+            return View();
+        }
+        public ActionResult Silicosis()
+        {
+            return View();
+        }
+        public ActionResult Mesothelioma()
+        {
+            return View();
+        }
+        public ActionResult Pneumoconiosis()
+        {
+            return View();
+        }
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+        public ActionResult Questionnaire()
+        {
+            return View();
+        }
+
+        public ActionResult test()
+        {
+            return View();
+        }
+    }
+
+    public class BasicAuthenticationAttribute : ActionFilterAttribute
+    {
+        public string BasicRealm { get; set; }
+        protected string Username { get; set; }
+        protected string Password { get; set; }
+
+        public BasicAuthenticationAttribute(string username, string password)
+        {
+            this.Username = "admin";
+            this.Password = "123";
+        }
+
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var req = filterContext.HttpContext.Request;
+            var auth = req.Headers["Authorization"];
+            if (!String.IsNullOrEmpty(auth))
+            {
+                var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(auth.Substring(6))).Split(':');
+                var user = new { Name = cred[0], Pass = cred[1] };
+                if (user.Name == Username && user.Pass == Password) return;
+            }
+            filterContext.HttpContext.Response.AddHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "Ryadel"));
+            /// thanks to eismanpat for this line: http://www.ryadel.com/en/http-basic-authentication-asp-net-mvc-using-custom-actionfilter/#comment-2507605761
+            filterContext.Result = new HttpUnauthorizedResult();
+        }
+    }
+
+}
---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5120App.Models;
+
+namespace FIT5120App.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private breathebelieveEntities db = new breathebelieveEntities();
+
+        // GET: Questions
+
+
+     
+        public JsonResult Question(List<string> data)
+        {
+            int length = data.Count;
+            var answers = db.Questions.ToList();
+            //[industry_name,x,y,z]
+            List<string> diseases = new List<string>();
+            List<string> symptoms = new List<string>();
+            string industry_name;
+
+            for (int y = 1; y < length; y++)
+            {
+
+                symptoms.Add(data[y]);
+
+            }
+            industry_name = data[0];
+
+            foreach (var i in answers)
+            {
+                for (int y = 0; y < length - 1; y++)
+                {
+
+                    if (i.Industry_name.Contains(industry_name) && i.Symptom.Contains(symptoms[y]))
+                    {
+
+                        diseases.Add(i.Disease_name);
+                        Console.WriteLine(i.Disease_name);
+                        break;
+                    }
+
+                }
+
+
+            }
+            return Json(new
+            {
+                list = diseases
+            });
+
+        }
+
+    }
+}
